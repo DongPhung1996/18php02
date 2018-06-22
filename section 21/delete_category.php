7a12da7bdf26c4658480e808722c1da03f92c6e7
= <=>
2018-06-22 22:43:07 +0700

    website tin tuc

--- section 21/delete_category.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'connectdb.php';
-	$id = $_GET['idDel'];
-	$sql = "DELETE FROM categories WHERE id = $id";
-	mysqli_query($conn,$sql);
-	//chuyen trang bang php
-	header("Location: list_category.php");
-
- ?>
--- section 21/delete_product.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'connectdb.php';
-	$id = $_GET['idDel'];
-	$sql = "DELETE FROM products WHERE id = $id";
-	mysqli_query($conn,$sql);
-	//chuyen trang bang php
-	header("Location: list_category.php");
-
- ?>
